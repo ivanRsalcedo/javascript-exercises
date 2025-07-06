e95e4469f87feba0d0e05ff68a1cadf6dca2be0f
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-06 17:05:50 -0400

    Complete exercise 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+    if (Number.isInteger(a) && Number.isInteger(b) && a >= 0 && b >= 0) {
+        let total = 0;
+        for (let i = Math.min(a, b); i <= Math.max(a, b); i++) {
+            total += i;
+        }
+        return total;
+    } else {
+        return 'ERROR';
+    }
 };
 
 // Do not edit below this line
