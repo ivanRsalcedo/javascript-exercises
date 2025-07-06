480d784aaeb70905f133fa69ea1b09e360c0be70
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-06 15:34:00 -0400

    Complete exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0)
+        return "ERROR";
+    let total = "";
+    for (let i = 0; i < num; i++)
+        total += string;
+    return total;
 };
 
 // Do not edit below this line
