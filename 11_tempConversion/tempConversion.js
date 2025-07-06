58271e2f399cbefb7b6f140b390a5dcd5f184729
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-06 18:41:12 -0400

    Complete exercise 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+// C = (F - 32) × 5/9
+// F = (C × 9/5) + 32
+
+const convertToCelsius = function(F) {
+  return decimalize((F - 32) * 5 / 9);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(C) {
+  return decimalize((C * 9 / 5) + 32);
 };
 
 // Do not edit below this line
@@ -9,3 +14,7 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+function decimalize(d) {
+  return Math.round(d * 10) / 10;
+}
