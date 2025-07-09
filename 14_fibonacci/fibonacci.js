4aa7817bc36aa5ca5e859b87535639db7f7d57e8
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-09 17:41:44 -0400

    Complete exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    n = Number(n);
+    if (n < 0) return "OOPS";
+    if (n === 0) return 0;
 
+    let a = 0;
+    let b = 1;
+
+    for (let i = 1; i < n; i++) {
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+
+    return b;
 };
 
 // Do not edit below this line
