67793b72159484659f9073cb9afccd0e0d061e22
Ivan Roderick Salcedo <nj.ivan.salcedo@gmail.com>
2025-07-09 17:59:35 -0400

    Complete exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,20 @@
-const findTheOldest = function() {
+const currentYear = new Date().getFullYear();
 
+const findTheOldest = function (array) {
+    return array.reduce((oldest, person) => getAge(person) > getAge(oldest) ? person : oldest);
 };
+
+function getAge(person) {
+    if ('yearOfDeath' in person)
+        return person.yearOfDeath - person.yearOfBirth;
+    else   
+        return currentYear - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
+// name: 'Carly',
+//     yearOfBirth: 1942,
+//         yearOfDeath: 1970,
